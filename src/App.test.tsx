51be0ebb72295e80a980df901b3f180c1b29fd5d
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2021-11-11 11:29:36 -0600

    Adding locks and package files and new tests and additions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders @ sign', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/@/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test('moves @ sign with key press', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/@/i);
+//   const location = linkElement.;
+//   userEvent.keyboard('{arrowleft}');
+//   console.log(location);
+//   console.log(screen.getByText(/@/i).ariaPosInSet);
+//   expect(location).toEqual(screen.getByText(/@/i).ariaPosInSet);
+// })
